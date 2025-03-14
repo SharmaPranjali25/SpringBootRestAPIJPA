bd5ba6b0d1eb009e6ad7d6fdcf45b2b8e718b82c
Pranjali Sharma <104714667+SharmaPranjali25@users.noreply.github.com>
2025-03-14 13:53:27 +0530

    Create CustomerDao.java

---
+++ src/main/java/com.test.dao/CustomerDao.java
@@ -0,0 +1,19 @@
+package com.test.dao;
+
+import java.util.List;
+
+import com.test.entity.Customer;
+
+public interface CustomerDao {
+//SERVICE WE PROVIDE TO CLIENT
+	
+	public Customer createCustomer(Customer cst);
+	public List<Customer> readAllCustomers();
+	
+	public List<Customer> updateCustomer(Customer cst);
+	
+	public List<Customer> deleteCustomer(int id);
+	
+	
+	
+}
